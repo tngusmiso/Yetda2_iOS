7d5c794c00137befddedafe5a0f01d858d1f3f03
tngusmiso <k1miso012@gmail.com>
2021-09-16 20:57:02 +0900

    프로젝트 생성

---
+++ README.md
@@ -0,0 +1,15 @@
+# 🎁 옛다 Yetda
+
+> 간단한 질문으로 생일 선물을 추천해 주는 서비스
+* 기존 iOS : https://github.com/Nexters/Yetda_iOS
+* Android : https://github.com/Nexters/Yetda_Android
+
+<br/>
+
+### ⭐️ Nexters
+http://teamnexters.com
+
+<br/>
+
+### ⭐️ UPF 2021FW
+https://www.unit.center/upf  
---
+++ Yetda2_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yetda2_iOS
+//
+//  Created by 임수현 on 2021/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
