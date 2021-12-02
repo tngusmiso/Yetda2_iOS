b8c3ae4e4303a1e85608b730712be97fb717b151
tngusmiso <k1miso012@gmail.com>
2021-12-02 22:34:46 +0900

    📝 (#13) 뒤로가기 팝업 파일 생성

---
+++ Yetda2_iOS/common/Views/AlertView/AnswerResetAlertView.swift
@@ -0,0 +1,20 @@
+//
+//  AnswerResetAlertView.swift
+//  Yetda2_iOS
+//
+//  Created by 임수현 on 2021/12/02.
+//
+
+import UIKit
+
+class AnswerResetAlertView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
